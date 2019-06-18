f58b560d1c0bf3343e80e368e7588b62c83357fd
Arturo92 <EamesAguilera@gmail.com>
2019-06-18 05:21:53 -0700

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,9 @@
+<?php
+    $hostname = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "db";
+    
+    $db = mysqli_connect($hostname, $username, $password, $database);
+    
+?>
